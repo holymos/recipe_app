12de9a96f9ae182169fa819ab99dc0c3848afbad
Moisés Pacífico <moises.pacifico@hotmail.com>
2024-04-03 20:42:10 -0300

    chore: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', '@holymos/eslint-config/react'],
+  rules: {
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          ['module', '/^@[a-z].*-/'],
+          '/^@[a-z][^\\-]*$/',
+          ['parent', 'sibling', 'index'],
+        ],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.ts',
+          '.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          '@components': './src/components',
+          '@contexts': './src/contexts',
+          '@hooks': './src/hooks',
+          '@screens': './src/screens',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
